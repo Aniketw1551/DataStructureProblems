27dabdc200280a653a5a50333324c73d278b2d55
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-27 20:03:24 +0530

    [Aniket] Adding CreatedLinkedList program

---
+++ LinkedList/CustomLinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class CustomLinkedList
+    {
+        public Node head;
+        //Method to Add Node in Linked List
+        public void AddNode(int data)
+        {
+            Node node = new Node(data);
+            
+            if (this.head == null) //condition to Check head and add first Node
+            {
+                this.head = node;
+            }
+            else
+            {
+                var lastNode = GetLastNode();
+                lastNode.next = node;
+            }
+            Console.WriteLine("Inserted into node: " + node.data);
+        }
+        public Node GetLastNode()
+        {
+            //Temp Node to work
+            Node temp = head;
+            //Loop to check next pointer in Node
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+    }
+}
